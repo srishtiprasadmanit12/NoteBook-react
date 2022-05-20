8bbef443290f4a8032d27a383c3bcbc4a6a3ef36
srishti <srishtiprasad500@gmail.com>
2022-05-20 13:10:58 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Header(){
+    return(
+        <header>
+            <h1>My Notebook</h1>
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>Note 1</h1>
+            <p>This is my first Note</p>
+        </div>
+    );
+}
+export default Note;
